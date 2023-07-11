5eb40400ae40870281dba020f8e1c704facefb6a
westshae <shaewest02@gmail.com>
2023-07-11 15:58:37 +1200

    Votelist command implemented

--- src/main/java/com/altoya/nationsrebuilt/commands/TownCommand.java
+++ src/main/java/com/altoya/nationsrebuilt/commands/TownCommand.java
@@ -37,6 +37,12 @@ public class TownCommand implements CommandExecutor {
       case "kick":
         this.townKickSubCommand(player,args);
         break;
+      case "votelist":
+        this.townVoteListSubCommand(player,args);
+        break;
+      case "vote":
+        // this.townVoteSubCommand(player, args);
+        break;
       default:
         return false;
     }
@@ -171,7 +177,6 @@ public class TownCommand implements CommandExecutor {
       Player currentPlayer = Bukkit.getPlayer(uuid);
       currentPlayer.sendMessage("A vote to invite a new player named \"" + Bukkit.getPlayer(inviteeUUID).getName() + "\" has been added. Check /town vote.");
     }
-
   }
   
   
@@ -242,6 +247,42 @@ public class TownCommand implements CommandExecutor {
       currentPlayer.sendMessage("A vote to kick a player named \"" + Bukkit.getPlayer(kickeeUUID).getName() + "\" has been added. Check /town vote.");
     }
 
+  }
+
+  private void townVoteListSubCommand(Player player, String[] args) {
+    if (!player.hasPermission("nationsrebuilt.town.votelist")){
+      player.sendMessage("No permission to run this command.");
+      return;
+    }
+
+    //Load players.yml data file.
+    File playersFile = new File(Bukkit.getServer().getPluginManager().getPlugin("nationsrebuilt").getDataFolder(), "players.yml");
+    FileConfiguration playersData = YamlConfiguration.loadConfiguration(playersFile);
+
+    UUID playerUUID = player.getUniqueId();
+
+    boolean hasTown =  playersData.getBoolean("players." + playerUUID.toString() + ".town.has");
+    if(!hasTown){
+      player.sendMessage("You have no town.");
+      return;
+    }
+
+    String townName = playersData.getString("players." + playerUUID.toString() + ".town.name");
+
+    //Load towns.yml data file.
+    File townsFile = new File(Bukkit.getServer().getPluginManager().getPlugin("nationsrebuilt").getDataFolder(), "towns.yml");
+    FileConfiguration townsData = YamlConfiguration.loadConfiguration(townsFile);
+
+    ArrayList<String> currentTownVotes = (ArrayList<String>) townsData.getStringList("towns." + townName + ".votes");
+
+    if(currentTownVotes.size() == 0){
+      player.sendMessage("There are no votes available in your town.");
+      return;
+    }
+
+    for(String vote: currentTownVotes){
+      player.sendMessage(vote);
+    }
   }
 
 
