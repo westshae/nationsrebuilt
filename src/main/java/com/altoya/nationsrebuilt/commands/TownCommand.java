efbc8eb9f36ad0428050d8c2a5b16bfcc5375e62
westshae <shaewest02@gmail.com>
2023-07-11 14:42:52 +1200

    Added town invite sub command

--- src/main/java/com/altoya/nationsrebuilt/commands/TownCommand.java
+++ src/main/java/com/altoya/nationsrebuilt/commands/TownCommand.java
@@ -2,6 +2,8 @@ package com.altoya.nationsrebuilt.commands;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 import java.util.regex.Pattern;
 
@@ -25,6 +27,9 @@ public class TownCommand implements CommandExecutor {
     switch (args[0].toLowerCase()) {
       case "create":
         this.townCreateSubCommand(player, args);
+        break;
+      case "invite":
+        this.townInviteSubCommand(player, args);
         break;
       case "help":
         this.helpSubCommand(player, args);
@@ -82,7 +87,7 @@ public class TownCommand implements CommandExecutor {
       return;
     }
     townsData.set("factions." + townName + ".name", townName);
-    townsData.set("factions." + townName + ".members", playerUUID.toString());
+    townsData.set("factions." + townName + ".members", new ArrayList<String>().add(playerUUID.toString()) );
     playersData.set("players." + playerUUID.toString() + ".town.has", true);
     playersData.set("players."+playerUUID.toString()+".town.name", townName);
 
@@ -99,6 +104,60 @@ public class TownCommand implements CommandExecutor {
 
     player.sendMessage("Your town, \"" + townName + "\" has been created.");
   }
+
+  private void townInviteSubCommand(Player player, String[] args) {
+    if(args.length != 2){
+      player.sendMessage("Must have 2 arguments. /town invite {player-name}");
+      return;
+    }
+    if (!player.hasPermission("nationsrebuilt.town.invite")){
+      player.sendMessage("No permission to run this command.");
+      return;
+    }
+
+    //Load players.yml data file.
+    File playersFile = new File(Bukkit.getServer().getPluginManager().getPlugin("nationsrebuilt").getDataFolder(), "towns.yml");
+    FileConfiguration playersData = YamlConfiguration.loadConfiguration(playersFile);
+
+    UUID inviteeUUID = Bukkit.getPlayer(args[1].toString()).getUniqueId();
+
+    boolean inviteeExists = playersData.contains("players." + inviteeUUID.toString());
+
+    if(!inviteeExists) {
+      player.sendMessage("The user you are trying to invite doesn't exist.");
+      return;
+    }
+
+    UUID playerUUID = player.getUniqueId();
+
+    boolean hasTown =  playersData.getBoolean("players." + playerUUID.toString() + ".town.has");
+    if(!hasTown){
+      player.sendMessage("You have no town.");
+      return;
+    }
+    String townName = playersData.getString("players." + playerUUID.toString() + ".town.name");
+
+    //Load towns.yml data file.
+    File townsFile = new File(Bukkit.getServer().getPluginManager().getPlugin("nationsrebuilt").getDataFolder(), "towns.yml");
+    FileConfiguration townsData = YamlConfiguration.loadConfiguration(townsFile);
+
+    @SuppressWarnings("unchecked")
+    ArrayList<String> currentTownVotes = (ArrayList<String>) townsData.getList("towns." + townName + ".votes");
+    currentTownVotes.add("vote:" + inviteeUUID.toString());
+    townsData.set("towns." + townName + ".votes", currentTownVotes);
+
+    @SuppressWarnings("unchecked")
+    ArrayList<String> currentTownMembers = (ArrayList<String>) townsData.getList("towns." + townName + ".members");
+
+    for(Object uuidString : currentTownMembers.toArray()){
+      if(!(uuidString instanceof String))continue;
+      UUID uuid = (UUID) uuidString;
+      Player currentPlayer = Bukkit.getPlayer(uuid);
+      currentPlayer.sendMessage("A vote to invite a new player named \"" + Bukkit.getPlayer(inviteeUUID).getName() + "\" has been added. Check /town vote.");
+    }
+
+  }
+
 
 
 }
