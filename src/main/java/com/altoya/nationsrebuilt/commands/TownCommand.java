3a37c77b6d532e33cabbfa2f48b98cfe2676d0ab
westshae <shaewest02@gmail.com>
2023-07-12 12:52:45 +1200

    Introduced action completing after votes

--- src/main/java/com/altoya/nationsrebuilt/commands/TownCommand.java
+++ src/main/java/com/altoya/nationsrebuilt/commands/TownCommand.java
@@ -102,7 +102,9 @@ public class TownCommand implements CommandExecutor {
     townsData.set("towns." + townName + ".name", townName);
     townsData.set("towns." + townName + ".members", memberList);
     townsData.set("towns." + townName + ".votes", new ArrayList<String>());
+    townsData.set("towns." + townName + ".invites", new ArrayList<String>());
     townsData.set("towns." + townName + ".votecount", 0);
+    townsData.set("towns." + townName + ".membercount", 1);
     playersData.set("players." + playerUUID.toString() + ".town.has", true);
     playersData.set("players."+playerUUID.toString()+".town.name", townName);
 
@@ -159,7 +161,7 @@ public class TownCommand implements CommandExecutor {
 
     int townVoteCount = townsData.getInt("towns." + townName + ".votecount");
 
-    currentTownVotes.add((townVoteCount + 1) + ":invite:" + inviteeUUID.toString());
+    currentTownVotes.add((townVoteCount + 1) + ":invite:" + inviteeUUID.toString() + ":1:0:0:" + playerUUID.toString());
     townsData.set("towns." + townName + ".votecount", (townVoteCount+1));
     townsData.set("towns." + townName + ".votes", currentTownVotes);
     
@@ -231,7 +233,7 @@ public class TownCommand implements CommandExecutor {
 
     int townVoteCount = townsData.getInt("towns." + townName + ".votecount");
 
-    currentTownVotes.add((townVoteCount + 1) + ":kick:" + kickeeUUID.toString());
+    currentTownVotes.add((townVoteCount + 1) + ":kick:" + kickeeUUID.toString() + ":1:0:0:" + playerUUID.toString());
     townsData.set("towns." + townName + ".votes", currentTownVotes);
     townsData.set("towns." + townName + ".votecount", (townVoteCount + 1));
     
@@ -321,12 +323,116 @@ public class TownCommand implements CommandExecutor {
     FileConfiguration townsData = YamlConfiguration.loadConfiguration(townsFile);
 
     ArrayList<String> currentTownVotes = (ArrayList<String>) townsData.getStringList("towns." + townName + ".votes");
+    String[] vote = null;
+    String voteForRemoval = null;
+    for(String currentvote : currentTownVotes){
+      String[] splitVote = currentvote.split(":");
+      int number = Integer.parseInt(splitVote[0]);
+      //if vote numbers match argument vote number
+      if(number != Integer.parseInt(args[1])) continue;
+      vote = splitVote;
+      voteForRemoval = currentvote;
+    }
+    if(vote == null || voteForRemoval == null) return;
+    currentTownVotes.remove(voteForRemoval);
+    for(String stringUUID : vote[6].split("=")){
+      if(UUID.fromString(stringUUID) == playerUUID){
+        player.sendMessage("You have already voted on this vote.");
+        return;
+      }
+    }
+
+    int voteNumber = Integer.parseInt(vote[0]);
+    String voteType = vote[1];
+    UUID voteUUID = UUID.fromString(vote[2]);
+
+    int voteForYes = Integer.parseInt(vote[3]);
+    int voteForNone = Integer.parseInt(vote[4]);
+    int voteForNo = Integer.parseInt(vote[5]);
+    vote[6] += "=" + playerUUID.toString();
 
 
+    String playerVote = args[2];
+    if(playerVote.toLowerCase().equals("yes")){
+      voteForYes += 1;
+    } else if (playerVote.toLowerCase().equals("none")){
+      voteForNone += 1;
+    } else{
+      voteForNo += 1;
+    }
 
+    String voteString = voteNumber + ":" + voteType + ":" + voteUUID.toString() + ":" + voteForYes  + ":" + voteForNone + ":" + voteForNo + ":" + vote[6];
+
+    int townPlayerCount = townsData.getInt("towns." + townName + "membercount");
+    int totalVotes = voteForYes + voteForNone + voteForNo;
+    boolean majorityVote = voteForYes >=(townPlayerCount/2 + 1);
+
+    if(majorityVote){
+      //Vote passes
+      //add new vote, announce vote, complete action
+      ArrayList<String> currentTownMembers = (ArrayList<String>) townsData.getStringList("towns." + townName + ".members");
+
+      for(String uuidString : currentTownMembers){
+        UUID uuid = UUID.fromString(uuidString);
+        Player currentPlayer = Bukkit.getPlayer(uuid);
+        currentPlayer.sendMessage("A vote to " + voteType + " a player named \"" + Bukkit.getPlayer(voteUUID).getName() + "\" has succeeded. The vote has now been completed with a final vote of " + voteForYes + ":" + voteForNone + ":" + voteForNo + ".");
+      }
+
+      switch(voteType.toLowerCase()){
+        case "invite":
+          this.voteInviteConfirmed(townName, voteUUID, townsData);
+          break;
+        case "kick":
+          this.voteKickConfirmed(townName, voteUUID, townsData, playersData);
+          break;
+        default:
+          break;
+      }
+    } 
+    else if (totalVotes != townPlayerCount && !majorityVote){
+      //Vote incomplete
+      currentTownVotes.add(voteString);
+    } 
+    else if (totalVotes == townPlayerCount && !majorityVote){
+      //Vote fails permanently
+      ArrayList<String> currentTownMembers = (ArrayList<String>) townsData.getStringList("towns." + townName + ".members");
+
+      for(String uuidString : currentTownMembers){
+        UUID uuid = UUID.fromString(uuidString);
+        Player currentPlayer = Bukkit.getPlayer(uuid);
+        currentPlayer.sendMessage("A vote to " + voteType + " a player named \"" + Bukkit.getPlayer(voteUUID).getName() + "\" has failed. The vote has now been removed with a final vote of " + voteForYes + ":" + voteForNone + ":" + voteForNo + ".");
+      }
+  
+    }
+
+    townsData.set("towns." + townName + ".votes", currentTownVotes);
+    try {
+      townsData.save(townsFile);
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
   }
 
+  private void voteInviteConfirmed(String townName, UUID uuid, FileConfiguration townsData){
+    ArrayList<String> currentTownInvites = (ArrayList<String>) townsData.getStringList("towns." + townName + ".invites");
 
+    currentTownInvites.add(uuid.toString());
+    townsData.set("towns." + townName + ".invites", currentTownInvites);
+    Player invitee = Bukkit.getPlayer(uuid);
+    invitee.sendMessage("You have been invited to the town \"" + townName + "\".");
+  }
 
+  private void voteKickConfirmed(String townName, UUID uuid, FileConfiguration townsData, FileConfiguration playersData){
 
+    ArrayList<String> currentTownMembers = (ArrayList<String>) townsData.getStringList("towns." + townName + ".members");
+    int currentMemberCount = townsData.getInt("towns." + townName + ".membercount");
+    currentTownMembers.remove(uuid.toString());
+    townsData.set("towns." + townName + ".members", currentTownMembers);
+    townsData.set("towns." + townName + ".membercount", currentMemberCount-1);
+
+    playersData.set("players." + uuid.toString() + ".town", null);
+
+    Player kickee = Bukkit.getPlayer(uuid);
+    kickee.sendMessage("You have been kicked from the town \"" + townName + "\".");
+  }
 }
